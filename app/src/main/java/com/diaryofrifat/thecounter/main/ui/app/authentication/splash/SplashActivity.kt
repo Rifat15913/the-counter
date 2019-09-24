6be731f8559e2216ea97cd0ecc724cd656ed8a9b
Rifat <rifat@setuptech.international>
2019-09-24 19:36:55 +0600

    Design splash page

--- app/src/main/java/com/diaryofrifat/thecounter/main/ui/app/authentication/splash/SplashActivity.kt
+++ app/src/main/java/com/diaryofrifat/thecounter/main/ui/app/authentication/splash/SplashActivity.kt
@@ -1,11 +1,7 @@
 package com.diaryofrifat.thecounter.main.ui.app.authentication.splash
 
-import com.diaryofrifat.thecounter.main.data.BaseRepository
-import com.diaryofrifat.thecounter.main.ui.app.authentication.welcome.WelcomeActivity
 import com.diaryofrifat.thecounter.main.ui.app.landing.container.ContainerActivity
 import com.diaryofrifat.thecounter.main.ui.base.component.BaseActivity
-import com.diaryofrifat.thecounter.utils.helper.Constants
-import com.diaryofrifat.thecounter.utils.helper.SharedPrefUtils
 
 class SplashActivity : BaseActivity<SplashMvpView, SplashPresenter>(), SplashMvpView {
     override val layoutResourceId: Int
@@ -16,34 +12,7 @@ class SplashActivity : BaseActivity<SplashMvpView, SplashPresenter>(), SplashMvp
     }
 
     override fun startUI() {
-        val isGoogleAuthOn =
-            SharedPrefUtils.readBoolean(Constants.PreferenceKeys.IS_GOOGLE_AUTH_SET_AND_ON)
-
-        val isGoogleAuthVerified =
-            SharedPrefUtils.readBoolean(Constants.PreferenceKeys.IS_GOOGLE_AUTH_VERIFIED)
-
-        val isLoggedIn =
-            SharedPrefUtils.readBoolean(Constants.PreferenceKeys.LOGGED_IN)
-
-        if (isGoogleAuthOn) {
-            if (isGoogleAuthVerified) {
-                ContainerActivity.startActivity(this)
-            } else {
-                if (isLoggedIn) {
-                    presenter.compositeDisposable.add(
-                        BaseRepository.on().logOut(this)
-                    )
-                } else {
-                    WelcomeActivity.startActivity(this)
-                }
-            }
-        } else {
-            if (isLoggedIn) {
-                ContainerActivity.startActivity(this)
-            } else {
-                WelcomeActivity.startActivity(this)
-            }
-        }
+        ContainerActivity.startActivity(this)
     }
 
     override fun stopUI() {
